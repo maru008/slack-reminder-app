f0b04d28f028508545f06a58dc02c37379f9110b
N.Yoshimaru <nydsa529422@gmail.com>
2024-05-29 15:00:59 +0900

    add days before

--- app.py
+++ app.py
@@ -32,23 +32,32 @@ def generate_slack_commands(text, date, time, when_list, channel_notify):
     date_obj = datetime.strptime(date, '%Y-%m-%d')
     for when in when_list:
         calculated_date = date_obj
+        days_before = 0
         if when == '1_day':
             calculated_date -= timedelta(days=1)
+            days_before = 1
         elif when == '2_days':
             calculated_date -= timedelta(days=2)
+            days_before = 2
         elif when == '3_days':
             calculated_date -= timedelta(days=3)
+            days_before = 3
         elif when == '1_week':
             calculated_date -= timedelta(weeks=1)
+            days_before = 7
         elif when == '2_weeks':
             calculated_date -= timedelta(weeks=2)
+            days_before = 14
         elif when == '3_weeks':
             calculated_date -= timedelta(weeks=3)
+            days_before = 21
         elif when == '1_month':
             calculated_date -= timedelta(days=30)
+            days_before = 30
         
         calculated_date_str = calculated_date.strftime('%Y-%m-%d')
-        commands.append(command_template.format(text=text, date=calculated_date_str, time=time))
+        text_with_days_before = f"{text} ({days_before}日前)"
+        commands.append(command_template.format(text=text_with_days_before, date=calculated_date_str, time=time))
     return commands
 
 
